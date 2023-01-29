eb1aacb402442cfce0ed647ab4f778e8d139da0c
s570908 <songohyoung@gmail.com>
2023-01-29 23:21:26 +0900

    상대방이 전송한 chat이 이벤트로 들어 왔을 때 mutateChat()으로 cache를 갱신하였다. 그러나 callback에 전달되는 chatData에 이벤트로 전송받은 chat을 넣어서 chatData를 리턴하였다. 이 cache를 읽고 있는 useSWR들은 chatData 값이 변화가 없음을 알고 state가 변하지 않게 되어 이를 사용하고 있는 컴토넌트는 리렌더링을 하지 않게 된다. 즉 그 컴포넌트가 리렌더링되게 하려면 새로운 값을 만들어서 리턴해 줘야한다. chatData를 immutable하게 유지하고 새로운 newChatData를 만들어서 리턴을 하면 해당 컴포넌트는 리렌더링을 하게 된다. 이를 위해 immer를 사용하였다.

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -17,6 +17,7 @@ import { toast } from 'react-toastify';
 import useSWR from 'swr';
 import useSWRInfinite from 'swr/infinite';
 import { setDateVarsList, getDateInVarsList } from '@utils/apollo';
+import produce from 'immer';
 
 const PAGE_SIZE = 20;
 const DirectMessage = () => {
@@ -147,7 +148,13 @@ const DirectMessage = () => {
         console.log('내가 아닌 상대방이 전송한 chat이 이벤트로 들어 왔다.');
         //setDateInVar(workspace, id);
         setDateVarsList(workspace, id);
-        mutateChat().then(() => {
+        mutateChat((chatData) => {
+          const newChaData = produce(chatData, (draft) => {
+            draft?.[0].unshift(data);
+          });
+          //chatData?.[0].unshift(data); // 가장 최신인 dm chat 1개(data)를 가장 최신 페이지(chatData?.[0])의 가장 맨 앞(unshift)에 넣는다.
+          return newChaData;
+        }, false).then(() => {
           if (scrollbarRef.current) {
             // client의 bottom: scrollbarRef.current.getClientHeight() + scrollbarRef.current.getScrollTop()
             // scroll의 bottom: scrollbarRef.current.getScrollHeight()
